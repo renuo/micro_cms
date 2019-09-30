9ba8009a12f655f271bdba426ff1de6289555faa
Lukas Bischof <lukas.bischof@renuo.ch>
2019-09-30 17:49:13 +0200

    Fix auth token

--- lib/micro_cms.rb
+++ lib/micro_cms.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 require 'micro_cms/engine'
-require 'micro_cms/authorization_token'
 
 module MicroCms
   mattr_accessor :ckeditor_configuration_file
   @ckeditor_configuration_file = nil
 
-  def self.authorization_token
-    AuthorizationToken.instance.token
-  end
+  mattr_accessor :authorization_token
+  @authorization_token = nil
 
   def self.configure
     yield self
