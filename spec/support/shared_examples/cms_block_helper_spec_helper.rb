be4f335ebc454ffed87326ef22ef616e652acaf8
Lukas Bischof <lukas.bischof@renuo.ch>
2019-09-30 18:08:36 +0200

    Add cms_content method

--- spec/support/shared_examples/cms_block_helper_spec_helper.rb
+++ spec/support/shared_examples/cms_block_helper_spec_helper.rb
@@ -2,7 +2,7 @@
 
 RSpec.shared_examples_for 'creates a new content block at the given path' do
   it 'creates a new content block at the given path', :aggregate_failures do
-    expect { cms_block }.to change(MicroCms::ContentBlock, :count).by 1
+    expect { subject_method }.to change(MicroCms::ContentBlock, :count).by 1
     expect(content_block.content).to eq content
     expect(content_block.path).to eq MicroCms::ContentBlock.format_path path
   end
