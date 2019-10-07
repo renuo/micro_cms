0955f70d10bf99ee0e5860bd7a4109a66c153c65
Lukas Bischof <lukas.bischof@renuo.ch>
2019-10-07 11:52:25 +0200

    Set loadpath of factories

--- lib/micro_cms/engine.rb
+++ lib/micro_cms/engine.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
+begin
+  require 'factory_bot_rails'
+rescue LoadError # rubocop:disable Lint/HandleExceptions
+end
+
 module MicroCms
   class Engine < ::Rails::Engine
+    FACTORIES = File.expand_path('../../spec/factories', __dir__).freeze
+
     isolate_namespace MicroCms
 
     config.generators do |generators|
@@ -19,6 +26,12 @@ module MicroCms
     initializer 'micro_cms.configure_authorization_token' do |_app|
       MicroCms.configure do |config|
         config.authorization_token = SecureRandom.urlsafe_base64
+      end
+    end
+
+    if defined? FactoryBotRails
+      initializer 'mirco_cms.set_factory_paths', after: 'factory_bot.set_factory_paths' do |_app|
+        FactoryBot.definition_file_paths << FACTORIES
       end
     end
   end
